b829c82ad7b41458a4ea2d176cc0adbe0f05fd14
Abhishek <abhishekd5841@gmail.com>
2024-06-09 17:35:25 +0530

    second commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+import AgraImage from './images/Agra.jpeg';
+import DarjeelingImage from './images/Darjeeling.jpeg'; 
+import GoaImage from './images/Goa.jpeg'; 
+import JaipurImage from './images/Jaipur.jpeg'; 
+import JaisalmerImage from './images/Jaisalmer.jpg'; 
+import KochiImage from './images/Kochi.jpeg'; 
+import LehImage from './images/leh.jpeg'; 
+import ManaliImage from './images/Manali.jpg'; 
+import VaranasiImage from './images/Varanasi.jpeg'; 
+import KolkataImage from './images/Kolkata.jpg'; 
+
+const data=[
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow",
+        image:AgraImage,
+        price:"28,305",
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color. At the center of its stately street grid (notable in India) stands the opulent, colonnaded City Palace complex. With gardens, courtyards and museums, part of it is still a royal residence.",
+        image:JaipurImage,
+        price:"82,735",
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda",
+        image:GoaImage,
+        price:"53,500",
+    },
+    {
+        id:4,
+        name:"Varanasi",
+        info:"Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the Ganges River’s sacred waters and perform funeral rites. Along the city's winding streets are some 2,000 temples, including Kashi Vishwanath, the “Golden Temple,” dedicated to the Hindu god Shiva",
+        image:VaranasiImage,
+        price:"27,890",
+    },
+    {
+        id:5,
+        name:"Darjeeling",
+        info:"Darjeeling is a town in India's West Bengal state, in the Himalayan foothills. Once a summer resort for the British Raj elite, it remains the terminus of the narrow-gauge Darjeeling Himalayan Railway, or “Toy Train,” completed in 1881. It's famed for the distinctive black tea grown on plantations that dot its surrounding slopes. Its backdrop is Mt. Kanchenjunga, among the world’s highest peaks.",
+        image:DarjeelingImage,
+        price:"32,305",
+    },
+    {
+        id:6,
+        name:"Jaisalmer",
+        info:"Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow",
+        image:JaisalmerImage,
+        price:"29,910",
+    },
+    {
+        id:7,
+        name:"Manali",
+        info:"Manali is a town, near Kullu town in Kullu district in the Indian state of Himachal Pradesh. It is situated in the northern end of the Kullu Valley, formed by the Beas River. Manali is named after Manu, the progenitor of humanity in Hinduism. The name Manali is regarded as the derivative of Manu-Alaya. The town is located in the Kullu district, approximately 270 kilometres. ",
+        image:ManaliImage,
+        price:"31,423",
+    },
+    {
+        id:8,
+        name:"Kochi",
+        info:"Kochi (also known as Cochin) is a city in southwest India's coastal Kerala state. It has been a port since 1341, when a flood carved out its harbor and opened it to Arab, Chinese and European merchants. Sites reflecting those influences include Fort Kochi, a settlement with tiled colonial bungalows and diverse houses of worship. Cantilevered Chinese fishing nets, typical of Kochi, have been in use for centuries.",
+        image:KochiImage,
+        price:"56,423",
+    },
+  {
+        id:9,
+        name:"Leh",
+        info:"Leh is a city in Ladakh in the Kashmir Kashmir region. It is the largest city and the joint capital of Ladakh. Leh, located in the Leh district, was also the historical capital of the Kingdom of Ladakh. The seat of the kingdom, Leh Palace, the former residence of the royal family of Ladakh, was built in the same style.",
+        image:LehImage,
+        price:"41,000",
+    },
+    {
+        id:10,
+        name:"Kolkata",
+        info:"Kolkata,[a] also known as Calcutta[b] (its official name until 2001), is the capital and largest city of the Indian state of West Bengal. It lies on the eastern bank of the Hooghly River, 80 km (50 mi) west of the border with Bangladesh. Kolkata is the regarded by many sources as the cultural capital of India and a historically and culturally significant city in the historic region of Bengal.[1][18][19] It is the second largest Bengali-speaking city in the world. It has the highest number of Nobel laureates among all cities in India.",
+        image:KolkataImage,
+        price:"49,570",
+    },
+];
+
+export default data;
